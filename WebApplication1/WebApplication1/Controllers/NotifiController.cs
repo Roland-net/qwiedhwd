09f760c520b14e82a34c482980f0473ba2d88128
fedorKiselev1 <146938471+fedorKiselev1@users.noreply.github.com>
2024-09-17 14:26:50 +0300

    qw

---
+++ WebApplication1/WebApplication1/Controllers/NotifiController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using WebApplication1.Models;
+
+namespace SocNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotifiController : ControllerBase
+    {
+        public RolandContext Context { get; }
+
+        public NotifiController(RolandContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Notification> Notification = Context.Notifications.ToList();
+            return Ok(Notification);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Notification? Notification = Context.Notifications.Where(x => x.NotificationId == id).FirstOrDefault();
+            if (Notification == null)
+            {
+                return BadRequest("Not Found");
+            }
+            return Ok(Notification);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Notification Notification)
+        {
+            Context.Notifications.Add(Notification);
+            Context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(Notification Notification)
+        {
+            Context.Notifications.Update(Notification);
+            Context.SaveChanges();
+            return Ok(Notification);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Notification? Notification = Context.Notifications.Where(x => x.NotificationId == id).FirstOrDefault();
+            Context.Notifications.Remove(Notification);
+            Context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace SocNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public RolandContext Context { get; }
+
+        public UserController(RolandContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<User> user = Context.Users.ToList();
+            return Ok(user);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            User? user = Context.Users.Where(x => x.UserId == id).FirstOrDefault();
+            if (user == null)
+            {
+                return BadRequest("Not Found");
+            }
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user)
+        {
+            Context.Users.Add(user);
+            Context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(User user)
+        {
+            Context.Users.Update(user);
+            Context.SaveChanges();
+            return Ok(user);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            User? user = Context.Users.Where(x => x.UserId == id).FirstOrDefault();
+            Context.Users.Remove(user);
+            Context.SaveChanges();
+            return Ok();
+        }
+    }
+}
