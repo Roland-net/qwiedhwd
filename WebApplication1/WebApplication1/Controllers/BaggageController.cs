17de7652b55130ba804cb60f0e96a43dc40e0433
angel007L <147161054+angel007L@users.noreply.github.com>
2024-09-20 09:48:07 +0300

    qw

---
+++ WebApplication1/WebApplication1/Controllers/BaggageController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace SocNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaggageController : ControllerBase
+    {
+        public RolandContext Context { get; }
+
+        public BaggageController(RolandContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Baggage> Baggage = Context.Baggages.ToList();
+            return Ok(Baggage);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Baggage? Baggage = Context.Baggages.Where(x => x.BaggageId == id).FirstOrDefault();
+            if (Baggage == null)
+            {
+                return BadRequest("Not Found");
+            }
+            return Ok(Baggage);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Baggage Baggage)
+        {
+            Context.Baggages.Add(Baggage);
+            Context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(Baggage Baggage)
+        {
+            Context.Baggages.Update(Baggage);
+            Context.SaveChanges();
+            return Ok(Baggage);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Baggage? Baggage = Context.Baggages.Where(x => x.BaggageId == id).FirstOrDefault();
+            Context.Baggages.Remove(Baggage);
+            Context.SaveChanges();
+            return Ok();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/flController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using WebApplication1.Models;
+
+namespace SocNet.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class flController : ControllerBase
+    {
+        public RolandContext Context { get; }
+
+        public flController(RolandContext context)
+        {
+            Context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Flight> Flight = Context.Flights.ToList();
+            return Ok(Flight);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            Flight? Flight = Context.Flights.Where(x => x.FlightId == id).FirstOrDefault();
+            if (Flight == null)
+            {
+                return BadRequest("Not Found");
+            }
+            return Ok(Flight);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Flight Flight)
+        {
+            Context.Flights.Add(Flight);
+            Context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update(Flight Flight)
+        {
+            Context.Flights.Update(Flight);
+            Context.SaveChanges();
+            return Ok(Flight);
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            Flight? Flight = Context.Flights.Where(x => x.FlightId == id).FirstOrDefault();
+            Context.Flights.Remove(Flight);
+            Context.SaveChanges();
+            return Ok();
+        }
+    }
+}
